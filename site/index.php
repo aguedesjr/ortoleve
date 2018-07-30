52a6d9f851233bdafc60018a9b72a18fc8c29014
agsoares <agsoares@gmail.com>
2018-07-30 19:52:04 -0300

    Alterando pre-loader

--- site/index.php
+++ site/index.php
@@ -67,11 +67,17 @@
 <body class="">
 <div id="wrapper">
   <!-- preloader -->
-  <div id="preloader">
+  <!-- <div id="preloader">
     <div id="spinner">
       <div class="preloader-dot-loading">
         <div class="cssload-loading"><i></i><i></i><i></i><i></i></div>
       </div>
+    </div>
+    <div id="disable-preloader" class="btn btn-default btn-sm">Disable Preloader</div>
+  </div> -->
+  <div id="preloader">
+    <div id="spinner">
+      <img src="images/preloaders/1.gif" alt="">
     </div>
     <div id="disable-preloader" class="btn btn-default btn-sm">Disable Preloader</div>
   </div>
