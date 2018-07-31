ddd79e9fb9ce95b86d9c2fac3fbbde06d93d8f33
agsoares <agsoares@gmail.com>
2018-07-30 23:13:58 -0300

    Finalizado o rodape

---
+++ site/footer.php
@@ -0,0 +1,20 @@
+<footer id="footer" class="footer">
+    
+    <div class="footer-bottom">
+      <div class="container pt-20 pb-20">
+        <div class="row">
+          <div class="col-md-6 sm-text-center">
+            <p class="font-13 text-black-777 m-0">Copyright &copy;<?php echo date("Y"); ?> Ortoleve. Todos os direitos reservados.</p>
+          </div>
+          <div class="col-md-6 text-right flip sm-text-center">
+            <div class="widget no-border m-0">
+              <ul class="styled-icons icon-dark icon-circled icon-sm">
+                <li><a href="https://www.facebook.com/ortoleve.centrodeortodontia"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="https://www.instagram.com/ortolevecentrodeortodontia"><i class="fa fa-instagram"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
--- site/index.php
+++ site/index.php
@@ -505,30 +505,8 @@
   <!-- end main-content -->
 
   <!-- Footer -->
-  <footer id="footer" class="footer">
-    
-    <div class="footer-bottom">
-      <div class="container pt-20 pb-20">
-        <div class="row">
-          <div class="col-md-6 sm-text-center">
-            <p class="font-13 text-black-777 m-0">Copyright &copy;<?php echo date("Y"); ?> Ortoleve. Todos os direitos reservados.</p>
-          </div>
-          <div class="col-md-6 text-right flip sm-text-center">
-            <div class="widget no-border m-0">
-              <ul class="styled-icons icon-dark icon-circled icon-sm">
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                <li><a href="#"><i class="fa fa-skype"></i></a></li>
-                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
-                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
+  <?php include 'footer.php';?>
+  
   <a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
 </div>
 <!-- end wrapper -->
