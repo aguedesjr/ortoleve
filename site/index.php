1f8546c2bf3480731af330ae24231293996172ec
aguedesjr <agsoares@gmail.com>
2018-08-04 22:38:32 -0300

    Inicio do ajuste do index

--- site/index.php
+++ site/index.php
@@ -473,7 +473,7 @@
     </section>
     
     <!-- Section: About -->
-    <section>
+    <!-- <section>
       <div class="container">
         <div class="row">
           <div class="col-md-12">
@@ -497,6 +497,28 @@
             <h5>Sub Heading</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.</p>
             <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+          </div>
+        </div>
+      </div>
+    </section> -->
+    
+    <!-- Section: about -->
+    <section id="about">
+      <div class="container">
+        <div class="section-content">
+          <div class="row">
+            <div class="col-md-5">
+              <div class="row">
+                <img src="images/quem-somos-sorriso.png" alt="">
+              </div>
+            </div>
+            <div class="col-md-7">
+              <h2 class="font-size-38 mt-0">O valor do <span class="text-theme-colored">sorriso</span>.</h2>
+              <li><ol>Melhora a qualidade de vida;</ol></li>
+              <li><ol>Melhora a autoestima;</ol></li>
+              <li><ol>Melhora os relacionamentos;</ol></li>
+              <li><ol>Amplia as possibilidades no trabalho.</ol></li>
+            </div>
           </div>
         </div>
       </div>
