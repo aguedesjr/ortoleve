3626caadd2b8258202dd95f50862a6473d4c3377
agsoares <agsoares@gmail.com>
2018-08-08 23:15:35 -0300

    Alterado menu para ficar tudo em um nivel

--- site/menu.php
+++ site/menu.php
@@ -25,16 +25,21 @@
           <nav id="menuzord-right" class="menuzord orange no-bg"><a class="menuzord-brand pull-left flip mb-15" href="index.php"><img src="images/logo-menu.png" alt=""></a>
             <ul class="menuzord-menu">
               <!-- <li class="active"><a href="#home">Home</a></li>-->
-              <li><a href="#">Ortoleve</a>
+              <!-- <li><a href="#">Ortoleve</a>
                 <ul class="dropdown">
                   <li><a href="sobre.php">Quem somos</a></li>
                   <li><a href="servicos.php">Serviços</a></li>
                   <li><a href="profissionais.php">Profissionais</a></li>
                   <li><a href="ortoleve50.php">Ortoleve-50</a></li>
-                  <!-- <li><a href="planos.php">Planos</a></li>  -->
+                  <li><a href="planos.php">Planos</a></li>
                   <li><a href="fotos.php">Espaço Ortoleve</a></li>
                 </ul>
-              </li>
+              </li>-->
+              <li><a href="sobre.php">Quem somos</a></li>
+              <li><a href="servicos.php">Serviços</a></li>
+              <li><a href="profissionais.php">Profissionais</a></li>
+              <li><a href="ortoleve50.php">Ortoleve-50</a></li>
+              <li><a href="fotos.php">Espaço Ortoleve</a></li>
               <li><a href="contato.php">Contato</a></li>
               <li><a href="login.php">Área do Cliente</a></li>
             </ul>
