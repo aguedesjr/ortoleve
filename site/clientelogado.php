e8a01976ddf1bc316f9891db49b31a495372675a
agsoares <agsoares@gmail.com>
2018-10-18 17:14:31 -0300

    Retirado a tabela do CRO da pagina do cliente logado

--- site/clientelogado.php
+++ site/clientelogado.php
@@ -215,9 +215,9 @@ td#titulo {
                 <h3 class="line-bottom mt-0 mb-30">
                     <table>
                     	<tr><td width="30%">Tabela de Preços</td>
-                    	<td align="right" width="70%"><a href="http://www.sodf.org.br/site/Arquivos/TABELA%20VRPO%202016.pdf" class="btn btn-theme-colored"><i class="fa fa-file-pdf"></i> Valor Tab.</a></td>
-                    	<td><p>&nbsp;</p></td>
-                    	<td><a href="alterarsenha.php" class="btn btn-theme-colored"><i class="fa fa-key"></i> Senha</a></td>
+                    	<!-- <td align="right" width="70%"><a href="http://www.sodf.org.br/site/Arquivos/TABELA%20VRPO%202016.pdf" class="btn btn-theme-colored"><i class="fa fa-file-pdf"></i> Valor Tab.</a></td>
+                    	<td><p>&nbsp;</p></td> -->
+                    	<td align="right" width="70%"><a href="alterarsenha.php" class="btn btn-theme-colored"><i class="fa fa-key"></i> Senha</a></td>
                     	<td><p>&nbsp;</p></td>
                     	<td><a href="logout.php" class="btn btn-theme-colored"><i class="fa fa-sign-out"></i> Sair</a></td></tr>
                     </table>
@@ -265,8 +265,8 @@ td#titulo {
                 <!-- Contact Form -->
                 <table>
                     <tr>
-                        <td><a href="http://www.sodf.org.br/site/Arquivos/TABELA%20VRPO%202016.pdf" class="btn btn-theme-colored"><i class="fa fa-file-pdf"></i> CRO-DF</a></td>
-                        <td><p>&nbsp;</p></td>
+                        <!-- <td><a href="http://www.sodf.org.br/site/Arquivos/TABELA%20VRPO%202016.pdf" class="btn btn-theme-colored"><i class="fa fa-file-pdf"></i> CRO-DF</a></td>
+                        <td><p>&nbsp;</p></td> -->
                         <td><a href="alterarsenha.php" class="btn btn-theme-colored"><i class="fa fa-key"></i> Senha</a></td>
                         <td><p>&nbsp;</p></td>
                         <td><a href="logout.php" class="btn btn-theme-colored"><i class="fa fa-sign-out"></i> Sair</a></td>
